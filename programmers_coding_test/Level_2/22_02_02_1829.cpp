d778a7170798b2850079dbdadc922d9e703efe3c
nyw <skdud@ajou.ac.kr>
2022-02-02 23:50:06 +0900

    220202. solved problems - 1

---
+++ programmers_coding_test/Level_2/22_02_02_1829.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+void dfs(int cur) {
+
+}
+
+vector<int> solution(int m, int n, vector<vector<int>> picture) {
+    int number_of_area = 0;
+    int max_size_of_one_area = 0;
+
+    vector<int> answer(2);
+    answer[0] = number_of_area;
+    answer[1] = max_size_of_one_area;
+    return answer;
+}
