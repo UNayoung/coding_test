e009535278cbd5093dcabac19a3836b4a66e4c26
nyw <skdud@ajou.ac.kr>
2022-03-10 23:55:28 +0900

    220310. solved problems - 2

--- programmers_coding_test/Level_2/22_03_09_17680.py
+++ programmers_coding_test/Level_2/22_03_09_17680.py
@@ -1,20 +1,18 @@
 def solution(cacheSize, cities):
     answer = 0
     cache = []
-    fre = []
     if cacheSize == 0:
         return len(cities * 5)
     for i in cities:
         if i.lower() in cache:
+            temp = cache.index(i.lower())
+            cache.append(i.lower())
+            cache.pop(temp)
             answer += 1
-            fre[cache.index(i.lower())] += 1
         else:
             if len(cache) >= cacheSize:
-                temp = fre.index(min(fre))
-                cache.pop(temp)
-                fre.pop(temp)
+                cache.pop(0)
             cache.append(i.lower())
-            fre.append(0)
             answer += 5
     return answer
 
