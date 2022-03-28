36be0b57fdc7c2746cccb1183664b2d5672fd146
nyw <skdud@ajou.ac.kr>
2022-03-28 23:49:46 +0900

    220328. solved problems - 1

---
+++ programmers_coding_test/Level_2/22_03_28_12899.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = ""
+    otf = ['1', '2', '4']
+    while n:
+        tmp = (n - 1) % 3
+        n = (n - 1) // 3
+        answer += otf[tmp]
+    return answer[::-1]
+
+print(solution(4))
