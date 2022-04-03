8d9ebad6eae968268d34269da2792ead4fcc62fd
nyw <skdud@ajou.ac.kr>
2022-04-03 23:21:11 +0900

    220403. solved problems - 1

---
+++ programmers_coding_test/Level_2/22_04_03_42860.py
@@ -0,0 +1,33 @@
+def solution(name):
+    answer = 0
+    origin = ''
+    cur = 0
+    for i in range(len(name)):
+        origin += 'A'
+    while True:
+        temp1 = ord(name[cur]) - ord('A')
+        temp2 = ord('Z') - ord(name[cur])
+        if temp1 < temp2:
+            answer += temp1
+        else:
+            answer += (temp2 + 1)
+        origin = origin[0:cur] + name[cur] + origin[cur+1:len(name)]
+        if name == origin:
+            return answer
+        left = cur
+        right = cur
+        while True:
+            left -= 1
+            right += 1
+            if left < 0:
+                left = len(name) - 1
+            if right > (len(name) - 1):
+                right = 0
+            answer += 1
+            if origin[right] != name[right]:
+                cur = right
+                break
+            if origin[left] != name[left]:
+                cur = left
+                break
+    return answer
