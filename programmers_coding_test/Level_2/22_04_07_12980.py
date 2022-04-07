92a2561ff7a3ffc9688598277df851e8883f2dae
nyw <skdud@ajou.ac.kr>
2022-04-07 23:33:56 +0900

    220407. solved problems - 1

---
+++ programmers_coding_test/Level_2/22_04_07_12980.py
@@ -0,0 +1,10 @@
+def solution(n):
+    ans = 1
+    while True:
+        if n == 1:
+            break
+        q, r = divmod(n, 2)
+        n = q
+        if r == 1:
+            ans += 1
+    return ans
