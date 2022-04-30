77c401c388a8d811faf529d68784949af76f4af5
nyw <skdud@ajou.ac.kr>
2022-04-30 22:35:51 +0900

    220430. solved problems - 1

---
+++ programmers_coding_test/Level_2/22_04_30_12905.py
@@ -0,0 +1,12 @@
+def solution(board):
+    answer = 1234
+
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    print('Hello Python')
+
+    for i in board:
+        print(i)
+
+    return answer
+
+print(solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]]))
