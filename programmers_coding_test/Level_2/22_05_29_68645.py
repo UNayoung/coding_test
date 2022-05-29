a22d59f9a561654e4b7ce08ccd6ce90f8fddf925
nyw <skdud@ajou.ac.kr>
2022-05-29 21:15:54 +0900

    220529. solved problems - 5

---
+++ programmers_coding_test/Level_2/22_05_29_68645.py
@@ -0,0 +1,10 @@
+def solution(n):
+    answer = []
+    graph = [[0]*i for i in range(1, n+1)]
+    start = n
+    for i in range(n, 0, -3):
+        for j in range(i):
+
+    return answer
+
+print(solution(6))
