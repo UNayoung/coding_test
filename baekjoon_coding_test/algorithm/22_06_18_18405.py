c80e901f4b55f22fb992bdb20e28b112a12dc479
UNayoung <skdud@ajou.ac.kr>
2022-06-18 23:18:56 +0900

    220618. solved problems - 3

---
+++ baekjoon_coding_test/algorithm/22_06_18_18405.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+def bfs():
+    queue = deque()
+
+def solution():
+    n, k = map(int, input().split())
+
+solution()
---
+++ programmers_coding_test/Level_2/22_06_18_60057.py
@@ -0,0 +1,9 @@
+def solution(s):
+    answer = -1
+    for i in range(1, len(s) // 2 + 1):        
+        answer_temp = ''
+        current = s[:i]
+        count = 0
+
+
+print(solution("aabbaccc"))
