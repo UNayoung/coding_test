954a006b65507c1bb8ca1ad8a4f1c80dae13e710
UNayoung <skdud@ajou.ac.kr>
2022-06-26 12:21:39 +0900

    220626. solved problems - 2

---
+++ baekjoon_coding_test/algorithm/22_06_26_11403.py
@@ -0,0 +1,17 @@
+import sys
+
+def solution():
+    input = sys.stdin.readline
+    n = int(input())
+    graph = []
+    for _ in range(n):
+        graph.append(list(map(int, input().split())))
+    for k in range(n):
+        for a in range(n):
+            for b in range(n):
+                if graph[a][k] == 1 and graph[k][b] == 1:
+                    graph[a][b] = 1
+    for i in graph:
+        print(*i)
+
+solution()
