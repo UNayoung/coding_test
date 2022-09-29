a9ad07904f3d1020f3619d54af022dd1fe44aae0
UNayoung <skdud@ajou.ac.kr>
2022-09-29 23:27:21 +0900

    220929. solved problems - 1

---
+++ programmers_coding_test/Level_2/22_09_29_00002.py
@@ -0,0 +1,24 @@
+from typing import List
+
+new_dic = []
+
+def make(cur, remain, k):
+    if remain == "":
+        global new_dic
+        new_dic.append(cur)
+        return
+    for i in range(1, min(len(remain), k) + 1):
+        make(cur + remain[:i], remain[i:], k)
+        make(cur + ".", remain[i:], k)
+
+def solution(k: int, dic: List[str], chat: str) -> str:
+    chat_word = chat.split()
+    for i in dic:
+        make("", i, k)
+    dic += new_dic
+    for i in range(len(chat_word)):
+        if chat_word[i] in dic:
+            chat_word[i] = "#" * (len(chat_word[i]))
+    return " ".join(chat_word)
+
+print(solution(2, ["slang", "badword"], "badword ab.cd bad.ord .word sl.. bad.word"))
