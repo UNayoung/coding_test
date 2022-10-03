ec1fc6d6d11bcbea422b964d1d751a589751a003
UNayoung <skdud@ajou.ac.kr>
2022-10-03 23:54:50 +0900

    221003. solved problems - 1

---
+++ programmers_coding_test/Level_2/22_10_03_92335.py
@@ -0,0 +1,26 @@
+def nTok(n, k):
+    n1, n2 = divmod(n, k)
+    if n1 == 0:
+        return str(n2)
+    else:
+        return nTok(n1, k) + str(n2)
+
+def isPrime(n):
+    if n > 1:
+        for i in range(2, n):
+            if (n % i) == 0:
+                return False
+        return True
+    else:
+        return False
+
+def solution(n, k):
+    answer = 0
+    convert = nTok(n, k)
+    arr = convert.split("0")
+    for i in arr:
+        if isPrime(int(i)):
+            answer += 1
+    return answer
+
+print(solution(437674, 3))
