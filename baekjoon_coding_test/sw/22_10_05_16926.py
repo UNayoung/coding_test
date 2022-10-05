58a91f03809d534d1b080de6eefe863373a647fd
UNayoung <skdud@ajou.ac.kr>
2022-10-05 23:57:12 +0900

    221005. init problems - 1

---
+++ baekjoon_coding_test/sw/22_10_05_16926.py
@@ -0,0 +1,12 @@
+import sys
+
+def rotate(arr, n, m):
+    temp = arr[:]
+
+def solution():
+    input = sys.stdin.readline
+    n, m, r = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    print(arr)
+
+solution()
